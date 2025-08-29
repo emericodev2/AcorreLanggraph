1482bea7225d20e38022fedd7f0aa6d3045f7214
Eric <ericsee@staging.emerico.biz>
2025-08-29 11:04:55 +0800

    update4

---
+++ deps/dummy_package/__init__.py
@@ -0,0 +1,1 @@
+# Dummy package to satisfy LangGraph Cloud build requirements
---
+++ deps/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dummy-package",
+    version="0.1.0",
+    description="A dummy package to satisfy LangGraph Cloud build requirements",
+    packages=find_packages(),
+    install_requires=[],
+)
